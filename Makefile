4020c1746d020ac7c2381c93aeb1ae14b11f6901
Leonardo Rossi <leonardo.rossi@unipr.it>
2023-09-14 08:51:19 +0000

    Update on Overleaf.

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	python scripts/show_plot.py results/exp_1.csv images/exp_1.jpg
+	python scripts/show_plot.py results/exp_2.csv images/exp_2.jpg
+	python scripts/show_plot.py results/exp_3.csv images/exp_3.jpg
---
+++ scripts/show_plot.py
@@ -0,0 +1,45 @@
+
+"""Plot graph."""
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from math import floor
+
+# config
+
+filename = sys.argv[1]
+
+output = None
+if len(sys.argv) > 2:
+    output = sys.argv[2]
+
+#
+
+df = pd.read_csv(filename)
+
+df = df.apply(lambda x: x * 100 if x.name != 'epochs' else x, axis=0)
+
+ax = plt.gca()
+df.plot(kind='line', x='epochs', ax=ax)
+ax.set_ylabel('AUC')
+ax.yaxis.set_ticks(
+    np.arange(floor(min(df.iloc[:, 1:].min())),
+              round(max(df.iloc[:, 1:].max()))+0.5, 1.))
+ax.grid(True)
+
+colors = ['g', 'm', 'tab:orange', 'tab:cyan', 'b', 'y']
+markers = ['x', 'v', '1', '*', '_', '<']
+for i in range(0, len(ax.lines)):
+    # set color
+    ax.lines[i].set_color(colors[i])
+    ax.lines[i].set_marker(markers[i])
+
+# update legend
+ax.legend(handles=ax.lines)
+
+if output is None:
+    plt.show()
+else:
+    plt.savefig(output, dpi=300)
